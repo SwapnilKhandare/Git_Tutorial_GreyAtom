b5552021f4ad5817da9f49c0ab07411880fe736a
SwapnilKhandare <swapnil.khandare1984@gmail.com>
2018-07-07 16:48:44 +0530

    necessary

--- dummy_script.py
+++
@@ -1,9 +0,0 @@
-########### Create certain variable #############3
-
-first_var = 1
-second_var = 2
-fourth_var =3
-fifth_var = 4
-############  Do some computation ###################
-
-third_var = first_var + second_var
